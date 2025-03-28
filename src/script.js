a49ccfaae8a0a301f169c4154cbb58c70cc3aa29
k-yadav1710 <kartik9828787421@gmail.com>
2025-03-28 21:08:19 +0100

    30% done

---
+++ src/script.js
@@ -0,0 +1,17 @@
+// script.js
+function greet() {
+    alert("Hello from the external JavaScript file!");
+}
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'custom-purple-1': '#6f58e4',
+          'custom-purple-2': '#8481da',
+          'custom-purple-3': '#511ef2',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
